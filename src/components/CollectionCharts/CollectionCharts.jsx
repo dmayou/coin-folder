63b061b21596f1405641d53a1e0f14ab3264dc6d
David Mayou <davidpmayou@gmail.com>
2019-01-16 12:09:52 -0600

    stub bar chart

--- src/components/CollectionCharts/CollectionCharts.jsx
+++ src/components/CollectionCharts/CollectionCharts.jsx
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
 import { Bar, Pie } from 'react-chartjs-2'
 
-class CollectionCharts extends Component {
+class CollectionCharts extends Component { 
     render() {
         return(
         <div>
+            <Bar
+                data={{
+                    labels: [
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July"
+                        ], 
+                    datasets: [
+                        {
+                            label: 'Dataset 1',
+                            backgroundColor: "rgba(220,220,220,0.5)",
+                            data: [
+                                4,
+                                5,
+                                6,
+                                8,
+                                8,
+                                9,
+                                2,
+                            ]
+                        }, {
+                        hidden: true,
+                        label: 'Dataset 2',
+                        backgroundColor: "rgba(151,187,205,0.5)",
+                        data: [
+                            4,
+                            4,
+                            4,
+                            4,
+                            4,
+                            4,
+                            4
+                        ]
+                        }, {
+                            label: 'Dataset 3',
+                        backgroundColor: "rgba(151,187,205,0.5)",
+                        data: [
+                            4,
+                            4,
+                            4,
+                            4,
+                            4,
+                            4,
+                            4
+                        ]
+                    }
+                    ]
+                }}
+            />
             <Pie
                 options={
                     {
