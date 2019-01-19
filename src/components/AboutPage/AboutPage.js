780bcc766821fa132f9249bc5d6f01af8c7e474c
David Mayou <davidpmayou@gmail.com>
2019-01-19 09:04:56 -0600

    add text to About, fix bug importing withStyles in some components

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import 'typeface-roboto';
 
-const AboutPage = () => (
+const styles = theme => ({
+  text: {
+    margin: theme.spacing.unit,
+  }
+});
+
+const AboutPage = ({classes}) => (
   <div>
     <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
+      <Typography className={classes.text} variant={'h5'}>Coin Folder v. 0.5</Typography>
+      <Typography>Keep collecting!</Typography>
+      <Typography>&copy; David Mayou 2019</Typography>
+        Technologies Used:
+        React
     </div>
   </div>
 );
 
-export default AboutPage;
+export default withStyles(styles)(AboutPage);
