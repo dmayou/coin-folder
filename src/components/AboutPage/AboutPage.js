7b03211a54a79aa87044b1caa0858d094abbbf43
David Mayou <davidpmayou@gmail.com>
2019-01-19 10:31:39 -0600

    add tech text to About

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core/';
 import 'typeface-roboto';
 
 const styles = theme => ({
   text: {
     margin: theme.spacing.unit,
-  }
+    marginTop: theme.spacing.unit / 2,
+    marginBottom: theme.spacing.unit / 2,
+  },
+  title: {
+    margin: theme.spacing.unit,
+    marginBottom: 0,
+  },
+  list: {
+    marginLeft: 30,
+  },
+  footer: {
+    align: 'center',
+  },
 });
 
 const AboutPage = ({classes}) => (
   <div>
     <div>
-      <Typography className={classes.text} variant={'h5'}>Coin Folder v. 0.5</Typography>
-      <Typography>Keep collecting!</Typography>
-      <Typography>&copy; David Mayou 2019</Typography>
-        Technologies Used:
-        React
+      <Typography className={classes.title} variant={'h5'}>Coin Folder v. 0.5</Typography>
+      <Typography className={classes.title} variant={'h6'}>Technologies Used:</Typography>
+      <List className={classes.list}>
+        <ListItem><ListItemText>React</ListItemText></ListItem>
+        <ListItem><ListItemText>Redux</ListItemText></ListItem>
+        <ListItem><ListItemText>Redux-Saga</ListItemText></ListItem>
+        <ListItem><ListItemText>Material UI</ListItemText></ListItem>
+        <ListItem><ListItemText>Node.js</ListItemText></ListItem>
+        <ListItem><ListItemText>PostgreSQL</ListItemText></ListItem>
+        <ListItem><ListItemText>Passport</ListItemText></ListItem>
+        <ListItem><ListItemText>Chart.js</ListItemText></ListItem>
+        <ListItem><ListItemText>Nodemailer</ListItemText></ListItem>
+      </List>
+      <Typography className={classes.footer} align="center" variant={'h6'}>Keep collecting!</Typography>
+      <Typography className={classes.footer} align="center">Thanks to Vega cohort and Prime instructors!</Typography>
+      <Typography className={classes.footer} align="center">&copy; David Mayou 2019</Typography>
     </div>
   </div>
 );
